92de9c2aeacf1d47e2ab4d4d360436e5dad85a4f
dbanzz <dzn508@york.ac.uk>
2024-01-20 20:36:18 +0800

    read files

---
+++ untitled1.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+articles = pd.read_csv('D:/Skill Assessment2/articles.BACE1.csv')
+authors = pd.read_csv('D:/Skill Assessment2/authors.BACE1.csv')
+paper = pd.read_csv('D:/Skill Assessment2/paper_counts.csv')
+
+print(paper.info)
+
