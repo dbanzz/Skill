126a793fd24fb1e872e6c6a89dcfeb6c32f5cc2e
dbanzz <dzn508@york.ac.uk>
2024-02-02 04:51:35 +0800

    final step

--- Wei.py
+++ Wei.py
@@ -3,25 +3,23 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 articles = pd.read_csv('D:/Skill Assessment2/articles.BACE1.csv')
-authors = pd.read_csv('D:/Skill Assessment2/authors.BACE1.csv')
+authors = pd.read_csv('D:/Skill Assessment2/authors_updated.BACE1.csv')
 paper = pd.read_csv('D:/Skill Assessment2/paper_counts.csv')
 
 articles['AuthorFullname'] = articles['FirstAuthorForename'] + ' ' + articles['FirstAuthorLastname']
 authors['AuthorFullname'] = authors['AuthorForename'] + ' ' + authors['AuthorLastname']
 
 
-#Filter out all articles about 'BACE1'
-f_articles = articles[articles['Title'].str.contains('BACE1')]
 
 #Merge articles that meet the conditions into the m_author variable through the PMID
-m_authors = authors.merge(f_articles[['PMID']], on='PMID')
+m_authors = authors.merge(articles[['PMID']], on='PMID')
 
 #Count the total number of papers for each author in the m_author variable
 paper_counts = m_authors['AuthorFullname'].value_counts()
 
 #Count author's Time_Span
-f_articles['Year'] = pd.to_datetime(f_articles['Year'], format='%Y')
-author_y = m_authors.merge(f_articles[['PMID', 'Year']], on='PMID')
+articles['Year'] = pd.to_datetime(articles['Year'], format='%Y')
+author_y = m_authors.merge(articles[['PMID', 'Year']], on='PMID')
 time_span = author_y.groupby('AuthorFullname')['Year'].agg([min, max])
 time_span['TimeSpan'] = (time_span['max'] - time_span['min']).dt.days / 365
 
@@ -46,6 +44,14 @@ sns.barplot(x=t_authors['TimeSpan'], y=t_authors.index, color='darkred', ax=ax2)
 ax2.set_ylabel('') 
 ax2.set_xlabel('Time Span (years)')
 ax2.set_title('Time Span by Author')
+
+# Add start and end time labels to each bar of a time span bar chart
+for author in t_authors.index:
+    start_year = time_span.loc[author, 'min'].year
+    end_year = time_span.loc[author, 'max'].year
+    ax2.text(t_authors.loc[author, 'TimeSpan'] + 0.5, author, f'{end_year}', va='center', ha='left', color='black')
+    ax2.text(0, author, f'{start_year}', va='center', ha='right', color='black')
+    
 
 #Hide the y-axis label of the second subgraph to facilitate a clearer view of the visual chart
 ax2.tick_params(labelleft=False)
@@ -74,6 +80,34 @@ plt.ylabel('Number of Articles')
 plt.grid(True)
 plt.xticks(articles_per_year_without_2024.index, rotation=45)
 plt.show()
+
+
+# Since the execution environment does not have access to the actual data, we will use dummy data to create the scatter plot.
+
+# Create a sample dataframe to simulate the actual data
+sample_data = {
+    'Author': ['Robert Vassar', 'Riqiang Yan', 'Stefan F Lichtenthaler', 'Weihong Song',
+          'Jae Sue Choi', 'Hyun Ah Jung', 'Yong Shen', 'Bart De Strooper',
+          'Wanxia He', 'Kaj Blennow'],
+    'StartYear': [2001, 2001, 2003, 2004, 2009, 2009, 2007, 2003, 2004, 2007],
+    'EndYear': [2022, 2023, 2023, 2022, 2019, 2019, 2023, 2023, 2023, 2022],
+}
+df = pd.DataFrame(sample_data)
+
+# Generate the scatter plot
+plt.figure(figsize=(10, 8))
+for i, row in df.iterrows():
+    plt.scatter([row['StartYear'], row['EndYear']], [i, i], color='blue')  # Plot the start and end year for each author
+    plt.plot([row['StartYear'], row['EndYear']], [i, i], color='blue', linestyle='-', linewidth=2)  # Connect start and end year with a line
+
+plt.yticks(range(len(df)), df['Author'])
+# Set the x-axis labels to show each year from the earliest start year to the latest end year
+plt.xticks(range(df['StartYear'].min(), df['EndYear'].max() + 1),rotation=45)
+plt.xlabel('Year')
+plt.title('Time Span by Author')
+plt.grid(True)
+plt.show()
+
 
 
 
